af8f0e7f80e7e256d7dba4c6f52950fff93454a0
DruxAMB <druxambmarketing@gmail.com>
2024-10-10 20:31:57 +0100

    @druxamb: made changes to the UI

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div>
+      <span className="border-2 border-blue-600 rounded-full w-7 h-7"></span>
+    </div>
+  );
+};
+
+export default Loading;
