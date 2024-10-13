23cc15ce8313707318aebf0545af505751ee7d82
DruxAMB <druxambmarketing@gmail.com>
2024-10-13 03:17:19 +0100

    @druxamb: replace mux-player with react-player and configured the oAuth2

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -2,6 +2,7 @@
 
 import axios from "axios";
 import MuxPlayer from "@mux/mux-player-react";
+import Player from "react-player";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
@@ -19,7 +20,7 @@ interface VideoPlayerProps {
   isLocked: boolean;
   completeOnEnd: boolean;
   title: string;
-};
+}
 
 export const VideoPlayer = ({
   videoUrl,
@@ -34,12 +35,16 @@ export const VideoPlayer = ({
   const router = useRouter();
   const confetti = useConfettiStore();
 
+
   const onEnd = async () => {
     try {
       if (completeOnEnd) {
-        await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, {
-          isCompleted: true,
-        });
+        await axios.put(
+          `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+          {
+            isCompleted: true,
+          }
+        );
 
         if (!nextChapterId) {
           confetti.onOpen();
@@ -49,13 +54,13 @@ export const VideoPlayer = ({
         router.refresh();
 
         if (nextChapterId) {
-          router.push(`/courses/${courseId}/chapters/${nextChapterId}`)
+          router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
         }
       }
     } catch {
       toast.error("Something went wrong");
     }
-  }
+  };
 
   return (
     <div className="relative aspect-video">
@@ -67,23 +72,21 @@ export const VideoPlayer = ({
       {isLocked && (
         <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
           <Lock className="h-8 w-8" />
-          <p className="text-sm">
-            This chapter is locked
-          </p>
+          <p className="text-sm">This chapter is locked</p>
         </div>
       )}
       {!isLocked && (
-        <MuxPlayer
-          title={title}
-          className={cn(
-            !isReady && "hidden"
-          )}
-          onCanPlay={() => setIsReady(true)}
+        <Player
+          url={"https://www.youtube.com/live/95RXd8RSZr0?si=HpeD97a3t0Q_Jg7o"} // kcpele replace with videoUrl
+          className={cn(!isReady && "hidden")}
+          onReady={() => setIsReady(true)}
           onEnded={onEnd}
+          controls={true}
+          width="100%"
+          height="100%"
           autoPlay
-          playbackId={videoUrl}
         />
       )}
     </div>
-  )
-}
+  );
+};
