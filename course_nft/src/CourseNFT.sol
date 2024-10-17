c901b1c8ef374abf3b744a4b38eab24fdfbecfbd
DruxAMB <druxambmarketing@gmail.com>
2024-10-17 16:15:11 +0100

    onchainkit error

--- course_nft/src/CourseNFT.sol
+++ course_nft/src/CourseNFT.sol
@@ -3,35 +3,54 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract CourseNFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    // Remove the Counters usage and replace with a simple uint256
+    uint256 private _currentTokenId;
 
     // Mapping from token ID to course ID
     mapping(uint256 => uint256) private _courseIds;
     // Mapping from token ID to course URL
     mapping(uint256 => string) private _courseUrls;
 
-    constructor() ERC721("CourseNFT", "CNFT") {}
+    // Add a modifier to check if the token exists
+    modifier tokenExists(uint256 tokenId) {
+        require(
+            _ownerOf(tokenId) != address(0),
+            "CourseNFT: Query for nonexistent token"
+        );
+        _;
+    }
 
-    function mintCourseNFT(address student, uint256 courseId, string memory courseUrl) public onlyOwner returns (uint256) {
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
+    constructor() ERC721("CourseNFT", "CNFT") Ownable(msg.sender) {
+        // Initialize the _currentTokenId to 0
+        _currentTokenId = 0;
+    }
+
+    function mintCourseNFT(
+        address student,
+        uint256 courseId,
+        string memory courseUrl
+    ) public onlyOwner returns (uint256) {
+        // Increment the token ID manually
+        _currentTokenId++;
+        uint256 newTokenId = _currentTokenId;
         _safeMint(student, newTokenId);
         _courseIds[newTokenId] = courseId;
         _courseUrls[newTokenId] = courseUrl;
         return newTokenId;
     }
 
-    function getCourseId(uint256 tokenId) public view returns (uint256) {
-        require(_exists(tokenId), "CourseNFT: Course query for nonexistent token");
+    // Apply the tokenExists modifier to these functions
+    function getCourseId(
+        uint256 tokenId
+    ) public view tokenExists(tokenId) returns (uint256) {
         return _courseIds[tokenId];
     }
 
-    function getCourseUrl(uint256 tokenId) public view returns (string memory) {
-        require(_exists(tokenId), "CourseNFT: Course query for nonexistent token");
+    function getCourseUrl(
+        uint256 tokenId
+    ) public view tokenExists(tokenId) returns (string memory) {
         return _courseUrls[tokenId];
     }
 }
