8f3fef2aef9adc3be9298d6551af4a80d4b215cd
DruxAMB <druxambmarketing@gmail.com>
2024-10-19 18:48:44 +0100

    @druxamb: fixed clerk middleware redirect url error

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
-const isAdminRoute = createRouteMatcher(['/admin(.*)'])
-
-export default clerkMiddleware((auth, req) => {
-  // Restrict admin route to users with specific role
-  if (isAdminRoute(req)) auth().protect({ role: 'org:admin' })
-
-  // Restrict dashboard routes to signed in users
-  if (isDashboardRoute(req)) auth().protect()
-})
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+const isProtectedRoute = createRouteMatcher(["/"]);
+export default clerkMiddleware((auth, request) => {
+  if (isProtectedRoute(request)) {
+    auth().protect();
+  }
+  return NextResponse.next();
+});
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
