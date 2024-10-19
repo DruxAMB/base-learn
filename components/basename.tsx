a5cdaf8b43c75395c7ecf091cf63cbd4dfebf8d4
DruxAMB <druxambmarketing@gmail.com>
2024-10-19 20:34:09 +0100

    @druxamb: added basename formatting on users profile

--- components/basename.tsx
+++ components/basename.tsx
@@ -1,15 +1,22 @@
-import { useUser } from "@clerk/nextjs";
+import { useUser  } from "@clerk/nextjs";
+import { Name } from '@coinbase/onchainkit/identity';
 
 export default function Basename() {
-  const { user } = useUser();
+  const { user } = useUser ();
 
   const primaryWeb3Wallet = user?.primaryWeb3Wallet;
+  const address = primaryWeb3Wallet?.web3Wallet;
+
+  // Type guard function to check if the address is valid
+  function isValidAddress(addr: string | undefined): addr is `0x${string}` {
+    return typeof addr === 'string' && addr.startsWith('0x');
+  }
 
   return (
     <>
-      <main className="me-2">
+      <main className="me-2 m-auto">
         <p>
-          {primaryWeb3Wallet ? primaryWeb3Wallet.web3Wallet : "basename"}
+          {primaryWeb3Wallet ? <Name address={isValidAddress(address) ? address : null} /> : ""}
         </p>
       </main>
     </>
